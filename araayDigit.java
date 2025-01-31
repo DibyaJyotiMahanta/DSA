2a3af5b8203e876937e6bc0f06867a4c75c56808
DibyaJyotiMahanta <mahantasoham@gmail.com>
2025-01-31 19:47:34 +0530

    g

---
+++ araayDigit.java
@@ -0,0 +1,9 @@
+
+import java.util.Arrays;
+
+public class araayDigit{
+    public static void main(String[] args) {
+        Integer a[] = {1,3};
+        System.out.println(Arrays.asList(a).contains(1));
+    }
+}
